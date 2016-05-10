9d4410f28fd32fe264362634d6e6e9f5f80baa26
Adam Mucha <adam.mucha@pl.abb.com>
2016-05-10 10:57:29 +0200

    Initial commit

---
+++ src/main/java/com/solidbrain/russads/MuchaApplication.java
@@ -0,0 +1,12 @@
+package com.solidbrain.russads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuchaApplication.class, args);
+	}
+}
